c6e549cf23adf946685c019327bd14eb324171f7
Tomasz Łaz <Tom@MacBook-Pro-Tomasz-3.local>
2016-10-12 14:29:38 +0200

    lab01

--- kol_gr2.py
+++ kol_gr2.py
@@ -1,23 +1,45 @@
-#Write a library that contains a class that can represent any 2𝑥2 real matrice. 
-#Include two functions to calculate the sum and product of two matrices. 
-#Next, write a program that imports this library module and use it to perform calculations.
-#Examples:
-#
-# matrix_1 = Matrix(4,5,6,7)
-# matrix_2 = Matrix(2,2,2,1)
-#
-# matrix_3 = matrix_2.add(matrix_1)
-#
-#Try to expand your implementation as best as you can. 
-#Think of as many features as you can, and try implementing them.
-#(If you want you can expand implementation to NxN matrix.)
-#Make intelligent use of pythons syntactic sugar (overloading, iterators, generators, etc)
-#Most of all: CREATE GOOD, RELIABLE, READABLE CODE.
-#The goal of this task is for you to SHOW YOUR BEST python programming skills.
-#Impress everyone with your skills, show off with your code.
-#
-#When you are done upload this code to your github repository. 
-#The whole repository MUST be named "kol_gr2"! 
-#
-#Delete these comments before commit!
-#Good luck.
+#!/usr/bin/env python2.7
+
+class Matrix():
+    def __init__(self, *args):
+        argsLen=len(args)/2.
+        toArgsLen=0
+        if(argsLen%2 != 0):
+            raise Exception
+        argsLen=int(argsLen)
+        self.nr=argsLen
+        self.matx=[[ [] for i in range(self.nr)] for i in range(self.nr)]
+        for i in range(self.nr):
+            for j in range(self.nr):
+                self.matx[i][j]=args[toArgsLen]
+                toArgsLen+=1
+
+    def __str__(self):
+        matxStr=''
+        for i in range(self.nr):
+            for j in range(self.nr):
+                matxStr += (str(self.matx[i][j]) + " ")
+            matxStr+="\n"
+        return matxStr
+
+
+    def __add__(self,matx2):
+        if (len(self.matx) >= len(matx2.matx)):
+            nr = len(matx2.matx)
+        else:
+            nr=len(self.matx)
+        for i in range(nr):
+            for j in range(nr):
+                self.matx[i][j] += matx2.matx[i][j]
+        return self.matx
+
+
+# Testing code
+mtx=Matrix(5,1,3,2)
+mtx2=Matrix(10,10,10,10)
+print mtx
+print mtx2
+
+mtx+mtx2
+print (mtx)
+
